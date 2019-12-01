b1885374f98fd0f99bd9c20f512c3ab2716f32ed
Remco Harmsen <rmeoc@mailbox.org>
2019-12-01 17:25:27 +0100

    Configure the keyfile.

--- network.nix
+++ network.nix
@@ -4,5 +4,9 @@
     { 
 	    imports = [ ./configuration.nix ];
 	    deployment.targetHost = ???;
+      deployment.keys.client-session.keyFile = ./client-session.aes;
+      deployment.keys.client-session.user = "mywebsrv";
+      deployment.keys.client-session.group = "mywebsrv";
+      deployment.keys.client-session.permissions = "0400";
     };
 }
